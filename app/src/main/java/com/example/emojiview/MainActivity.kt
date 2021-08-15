c69a1a5d80389504c62e6eb6231e12414691cc59
HamzaAkram <hamzaakram.shyk17@gmail.com>
2021-08-15 16:26:54 +0500

    Checking ..........

--- app/src/main/java/com/example/emojiview/MainActivity.kt
+++ app/src/main/java/com/example/emojiview/MainActivity.kt
@@ -15,6 +15,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        // Checking...............
 //        blurView = BlurView(this)
 //        binding.parentLayout.viewTreeObserver.addOnGlobalLayoutListener {
 //         //   saveParentWidth(binding.parentLayout.width)
